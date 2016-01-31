65030431a35a1bbcb6c59108c07cc2a3b0a33981
guzietz <guzek.pwl@gmail.com>
2016-01-31 22:03:22 +0100

    data

--- qa_test_1.cs
+++ qa_test_1.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
 namespace selenium_test_01
 {
